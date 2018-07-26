50a32e5d0aac4e5bde8f19260633db81372488eb
letspython3x <letspython3x@gmail.com>
2018-07-26 09:55:31 +0530

    Add Employee Model for storing employee Details

--- payroll/source/models.py
+++ payroll/source/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    empid = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    designation = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    department = models.CharField(max_length=50)
+
+
+
